62111b12c3c82f47a7fdb7918d9e1787e226f385
SathishKumar63 <sathishjim@gmail.com>
2019-07-25 15:00:59 +0530

    Update users-routing.module.ts

--- src/app/modules/users/users-routing.module.ts
+++ src/app/modules/users/users-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { NewUserListComponent } from './new-user-list/new-user-list.component';
+
 const routes: Routes = [
   {
     path: '', component: UsersComponent, children: [
       {
         path: '', children: [
           { path: 'userslist', component: UsersListComponent },
-          { path: '', component: UsersListComponent }
+          { path: 'lists', component: NewUserListComponent },
+          { path: '', component: NewUserListComponent }
         ]
       }
     ]
